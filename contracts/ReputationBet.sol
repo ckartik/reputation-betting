72ac79d068111b101e0006b13e913e300940d64b
ckartik <k5chopra@uwaterloo.ca>
2022-04-24 06:33:46 +0200

    Adds everything.

--- contracts/ReputationBet.sol
+++ contracts/ReputationBet.sol
@@ -8,16 +8,21 @@ contract ReputationBet {
     // The list of addreseses the key address trusts.
     mapping (address=>address[]) TrustedAddresses;
     // The stake assosciated with 
-    mapping (address=>uint[]) StakedValues;
+    mapping (address=>uint256[]) StakedValues;
     // Addresses that trust the key address.
     mapping (address=>address[]) Stakers;
-    mapping (address=>uint) TotalStake;
-    mapping (address=>uint) TotalNumOfStakers;
-    mapping (address=>uint) Claimable;
+    mapping (address=>uint256) TotalStake;
+    mapping (address=>uint256) TotalNumOfStakers;
+    mapping (address=>uint256) Claimable;
 
     function claim() public {
         require(Claimable[msg.sender] > 0);
         payable(msg.sender).transfer(Claimable[msg.sender]);
+    }
+
+    function claimable() public view returns (uint256) {
+        console.log("SMART CONTRACT: SENDER %s ", msg.sender);
+        return Claimable[msg.sender];
     }
 
     function stakeAndTrust(address TrustedAddress) public payable {
@@ -30,11 +35,21 @@ contract ReputationBet {
 
         uint val = msg.value;
         for (uint i=0; i < TotalNumOfStakers[TrustedAddress]; i++){
-            Claimable[Stakers[TrustedAddress][i]] += StakedValues[TrustedAddress][i] / TotalStake[TrustedAddress];
+            console.log("SMART CONTRACT: I GET HERE", msg.sender);
+            console.log("Staked value:",StakedValues[TrustedAddress][i]);
+            console.log("Staked Value by person", StakedValues[TrustedAddress][i] << 10);
+            console.log("The Total staked on this individiual", TotalStake[TrustedAddress]);
+            console.log("big value", ((StakedValues[TrustedAddress][i] << 10) / TotalStake[TrustedAddress]));
+            
+            Claimable[Stakers[TrustedAddress][i]] += (((StakedValues[TrustedAddress][i] << 10) / TotalStake[TrustedAddress]) * TotalStake[TrustedAddress]) >> 10;
+            console.log("Loaded final value", Claimable[Stakers[TrustedAddress][i]]);
+            console.log("Loading address",Stakers[TrustedAddress][i]);
             val -= StakedValues[TrustedAddress][i] / TotalStake[TrustedAddress];
+
 
             // Here figure out the highest staker and dispurse funds to them.
         }
+        TotalNumOfStakers[TrustedAddress]+= 1;
     }
 
 
--- test/sample-test.js
+++ test/sample-test.js
@@ -6,12 +6,24 @@ describe("Deploy & Stake", function () {
     const RB = await ethers.getContractFactory("ReputationBet");
     const rb = await RB.deploy();
     await rb.deployed();
+    const prov = ethers.getDefaultProvider();
+
     const [owner, addr1, addr2] = await ethers.getSigners();
     // console.log(rb)
     // console.log(await owner.getBalance())
     const ad = await addr1.getAddress();
-    const stakeTx = await rb.stakeAndTrust(ad);
+    const ad2 = await addr2.getAddress();
+    const stakeTx = await rb.connect(owner).stakeAndTrust(ad, {value: ethers.utils.parseUnits("10", "wei")});
+    const value1 = await rb.connect(addr1).claimable();
+    const balance = await prov.getBalance(rb.address);
+    console.log(balance);
     await stakeTx.wait();
+    console.log(value1)
+    const stakeTx2 = await rb.connect(addr2).stakeAndTrust(ad, {value: ethers.utils.parseUnits("5","wei")});
+    await stakeTx2.wait();
+
+    const value2 = await rb.connect(owner).claimable();
+    console.log(value2)
 
     // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
 
