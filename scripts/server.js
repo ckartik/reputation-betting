d648f2f0758f1945629a573922862717eac95427
ckartik <k5chopra@uwaterloo.ca>
2022-04-24 06:50:33 +0200

    Brings up the basic accounts.

--- scripts/server.js
+++ scripts/server.js
@@ -1,1 +1,27 @@
 const { ethers } = require("hardhat");
+
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+
+function local_deploy() {
+	const rb =  ethers.getContractFactory("ReputationBet").then((Rep) =>  Rep.deploy())
+
+	const accounts = ethers.getSigners();
+
+	return {"contract": rb, "accounts": accounts}
+}
+
+pair = local_deploy()
+
+let rb = pair.contract
+let accounts_pr = pair.accounts
+
+app.get("/accounts", async (req, res) => {
+    const accounts = await accounts_pr;
+    const contract = await rb;
+
+    res.send(accounts.slice(10).map((v,idx) => Object({ref_id: idx, address: v.address})))
+})
+app.listen(3000)
