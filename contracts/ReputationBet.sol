3196041b54aeba23a3bc42d603a1c3e9b0a0e362
ckartik <k5chopra@uwaterloo.ca>
2022-04-24 04:52:50 +0200

    Push the progress.

--- contracts/ReputationBet.sol
+++ contracts/ReputationBet.sol
@@ -11,13 +11,23 @@ contract ReputationBet {
     mapping (address=>uint[]) StakedValues;
     // Addresses that trust the key address.
     mapping (address=>address[]) Stakers;
-
     mapping (address=>uint) TotalStake;
-    
+    mapping (address=>uint) TotalStakers;
+    mapping (address=>uint) Claimable;
+
     function stakeAndTrust(address TrustedAddress) public payable {
         console.log("SMART CONTRACT: SENDER %s ", msg.sender);
         TrustedAddresses[msg.sender].push(TrustedAddress);
         Stakers[TrustedAddress].push(msg.sender);
         StakedValues[TrustedAddress].push(msg.value);
+
+        TotalStake[TrustedAddress] += msg.value;
+
+        uint val = msg.value;
+        for (uint i=0; i < TotalStakers[TrustedAddress] - 1; i++){
+            val = val;
+        }
     }
+
+
 }
