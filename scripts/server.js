7c9ec631ab3ab6273a9f2ac25aa6c83cdaf8b05b
ckartik <k5chopra@uwaterloo.ca>
2022-04-24 07:13:22 +0200

    Send the code off.

--- scripts/server.js
+++ scripts/server.js
@@ -24,4 +24,27 @@ app.get("/accounts", async (req, res) => {
 
     res.send(accounts.slice(10).map((v,idx) => Object({ref_id: idx, address: v.address})))
 })
+
+app.post("/stake", async (req, res) => {
+    const accounts = await accounts_pr;
+    const contract = await rb;
+    
+    
+    const staker = accounts[req.body.staker]
+    const trustedAddress = accounts[req.body.addressToStake].address
+    const valueToStake = req.body.value
+    console.log(trustedAddress)
+    const stakeTx = await contract.connect(staker).stakeAndTrust(trustedAddress, {value: ethers.utils.parseUnits(valueToStake,"wei")});
+    await stakeTx.wait();
+    res.send(200)
+})
+app.get("/claimable", async (req, res) => {
+    const accounts = await accounts_pr;
+    const contract = await rb;
+
+    const account = accounts[req.body.account]
+    const v = await contract.connect(account).claimable();
+
+    res.send(v.toString())
+})
 app.listen(3000)
